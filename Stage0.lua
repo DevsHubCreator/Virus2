4b7b538107996c460f0a146ccbf203ab6619a0cb
DevsHubCreator <95543850+DevsHubCreator@users.noreply.github.com>
2022-12-23 20:44:56 +0300

    Add files via upload

---
+++ Stage0.lua
@@ -0,0 +1,232 @@
+-- (VOID) : Gui to Lua
+-- Version: 1.4
+
+-- Instances:
+
+local Windows = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local ImageLabel = Instance.new("ImageLabel")
+local Frame_2 = Instance.new("Frame")
+local Frame_3 = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+local ImageLabel_2 = Instance.new("ImageLabel")
+local UIStroke = Instance.new("UIStroke")
+local Frame_4 = Instance.new("Frame")
+local UIStroke_2 = Instance.new("UIStroke")
+local TextLabel_2 = Instance.new("TextLabel")
+local TextLabel_3 = Instance.new("TextLabel")
+local TextLabel_4 = Instance.new("TextLabel")
+local TextLabel_5 = Instance.new("TextLabel")
+local TextLabel_6 = Instance.new("TextLabel")
+local TextLabel_7 = Instance.new("TextLabel")
+local TextLabel_8 = Instance.new("TextLabel")
+local Frame_5 = Instance.new("Frame")
+local UIStroke_3 = Instance.new("UIStroke")
+local Frame_6 = Instance.new("Frame")
+local UIStroke_4 = Instance.new("UIStroke")
+local Frame_7 = Instance.new("Frame")
+local UIStroke_5 = Instance.new("UIStroke")
+local UIStroke_6 = Instance.new("UIStroke")
+
+--Properties:
+
+Windows.Name = "Windows"
+Windows.Parent = (game:GetService("CoreGui") or gethui())
+
+Frame.Parent = Windows
+Frame.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(2.27573285e-08, 0, -2.30495303e-08, 0)
+Frame.Size = UDim2.new(0, 1341, 0, 662)
+
+ImageLabel.Parent = Frame
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BackgroundTransparency = 1.000
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Position = UDim2.new(0, 0, -0.0966767371, 0)
+ImageLabel.Size = UDim2.new(0, 1341, 0, 726)
+ImageLabel.Image = "http://www.roblox.com/asset/?id=6122179491"
+
+Frame_2.Parent = ImageLabel
+Frame_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame_2.BorderSizePixel = 0
+Frame_2.Position = UDim2.new(0.221476525, 0, 0.235537186, 0)
+Frame_2.Size = UDim2.new(0, 746, 0, 448)
+
+Frame_3.Parent = Frame_2
+Frame_3.BackgroundColor3 = Color3.fromRGB(32, 40, 139)
+Frame_3.BorderSizePixel = 0
+Frame_3.Position = UDim2.new(0.0110207731, 0, 0.0167871881, 0)
+Frame_3.Size = UDim2.new(0, 728, 0, 22)
+
+TextLabel.Parent = Frame_3
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0.0314207636, 0, 0.18181853, 0)
+TextLabel.Size = UDim2.new(0, 691, 0, 14)
+TextLabel.ZIndex = 2
+TextLabel.Font = Enum.Font.Code
+TextLabel.Text = "YouTube - Broadcast Yourself. - Microsoft Internet Explorer"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextSize = 14.000
+TextLabel.TextXAlignment = Enum.TextXAlignment.Left
+
+ImageLabel_2.Parent = Frame_3
+ImageLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_2.BackgroundTransparency = 1.000
+ImageLabel_2.BorderSizePixel = 0
+ImageLabel_2.Position = UDim2.new(0.00273224711, 0, 0.136363626, 0)
+ImageLabel_2.Size = UDim2.new(0, 15, 0, 15)
+ImageLabel_2.Image = "rbxassetid://11899683579"
+ImageLabel_2.SliceScale = 2.000
+
+UIStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+UIStroke.Color = Color3.fromRGB(198, 198, 198)
+UIStroke.LineJoinMode = Enum.LineJoinMode.Miter
+UIStroke.Thickness = 2.000
+UIStroke.Parent = Frame_3
+
+Frame_4.Parent = Frame_2
+Frame_4.BackgroundColor3 = Color3.fromRGB(213, 213, 213)
+Frame_4.BorderSizePixel = 0
+Frame_4.Position = UDim2.new(0.0110207796, 0, 0.0658942983, 0)
+Frame_4.Size = UDim2.new(0, 728, 0, 22)
+
+UIStroke_2.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+UIStroke_2.Color = Color3.fromRGB(198, 198, 198)
+UIStroke_2.LineJoinMode = Enum.LineJoinMode.Miter
+UIStroke_2.Thickness = 2.000
+UIStroke_2.Parent = Frame_4
+
+TextLabel_2.Parent = Frame_4
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.BorderSizePixel = 0
+TextLabel_2.Position = UDim2.new(0.00944275875, 0, 0.136364326, 0)
+TextLabel_2.Size = UDim2.new(0, 75, 0, 14)
+TextLabel_2.ZIndex = 2
+TextLabel_2.Font = Enum.Font.Code
+TextLabel_2.Text = "大声笑(F)"
+TextLabel_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.TextSize = 17.000
+TextLabel_2.TextXAlignment = Enum.TextXAlignment.Left
+
+TextLabel_3.Parent = Frame_4
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.BackgroundTransparency = 1.000
+TextLabel_3.BorderSizePixel = 0
+TextLabel_3.Position = UDim2.new(0.112464741, 0, 0.136364326, 0)
+TextLabel_3.Size = UDim2.new(0, 75, 0, 14)
+TextLabel_3.ZIndex = 2
+TextLabel_3.Font = Enum.Font.Code
+TextLabel_3.Text = "哇哦(E)"
+TextLabel_3.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_3.TextSize = 17.000
+TextLabel_3.TextXAlignment = Enum.TextXAlignment.Left
+
+TextLabel_4.Parent = Frame_4
+TextLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.BackgroundTransparency = 1.000
+TextLabel_4.BorderSizePixel = 0
+TextLabel_4.Position = UDim2.new(0.203124076, 0, 0.136364326, 0)
+TextLabel_4.Size = UDim2.new(0, 75, 0, 14)
+TextLabel_4.ZIndex = 2
+TextLabel_4.Font = Enum.Font.Code
+TextLabel_4.Text = "非也(V)"
+TextLabel_4.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_4.TextSize = 17.000
+TextLabel_4.TextXAlignment = Enum.TextXAlignment.Left
+
+TextLabel_5.Parent = Frame_4
+TextLabel_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.BackgroundTransparency = 1.000
+TextLabel_5.BorderSizePixel = 0
+TextLabel_5.Position = UDim2.new(0.296530664, 0, 0.136364326, 0)
+TextLabel_5.Size = UDim2.new(0, 100, 0, 14)
+TextLabel_5.ZIndex = 2
+TextLabel_5.Font = Enum.Font.Code
+TextLabel_5.Text = "男人野兔停止(A)"
+TextLabel_5.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_5.TextSize = 17.000
+TextLabel_5.TextXAlignment = Enum.TextXAlignment.Left
+
+TextLabel_6.Parent = Frame_4
+TextLabel_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_6.BackgroundTransparency = 1.000
+TextLabel_6.BorderSizePixel = 0
+TextLabel_6.Position = UDim2.new(0.451750457, 0, 0.136364326, 0)
+TextLabel_6.Size = UDim2.new(0, 68, 0, 14)
+TextLabel_6.ZIndex = 2
+TextLabel_6.Font = Enum.Font.Code
+TextLabel_6.Text = "ビデオツ(T)"
+TextLabel_6.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_6.TextSize = 17.000
+TextLabel_6.TextXAlignment = Enum.TextXAlignment.Left
+
+TextLabel_7.Parent = Frame_4
+TextLabel_7.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_7.BackgroundTransparency = 1.000
+TextLabel_7.BorderSizePixel = 0
+TextLabel_7.Position = UDim2.new(0.57400322, 0, 0.136364326, 0)
+TextLabel_7.Size = UDim2.new(0, 68, 0, 14)
+TextLabel_7.ZIndex = 2
+TextLabel_7.Font = Enum.Font.Code
+TextLabel_7.Text = "人間だ(H)"
+TextLabel_7.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_7.TextSize = 17.000
+TextLabel_7.TextXAlignment = Enum.TextXAlignment.Left
+
+TextLabel_8.Parent = Frame_4
+TextLabel_8.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_8.BackgroundTransparency = 1.000
+TextLabel_8.BorderSizePixel = 0
+TextLabel_8.Position = UDim2.new(0.00944275875, 0, 1.13636434, 0)
+TextLabel_8.Size = UDim2.new(0, 66, 0, 14)
+TextLabel_8.ZIndex = 2
+TextLabel_8.Font = Enum.Font.Code
+TextLabel_8.Text = "愚か者(D)"
+TextLabel_8.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_8.TextSize = 17.000
+TextLabel_8.TextXAlignment = Enum.TextXAlignment.Left
+
+Frame_5.Parent = Frame_2
+Frame_5.BackgroundColor3 = Color3.fromRGB(213, 213, 213)
+Frame_5.BorderSizePixel = 0
+Frame_5.Position = UDim2.new(0.0110207796, 0, 0.11500144, 0)
+Frame_5.Size = UDim2.new(0, 728, 0, 22)
+
+UIStroke_3.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+UIStroke_3.Color = Color3.fromRGB(198, 198, 198)
+UIStroke_3.LineJoinMode = Enum.LineJoinMode.Miter
+UIStroke_3.Thickness = 2.000
+UIStroke_3.Parent = Frame_5
+
+Frame_6.Parent = Frame_2
+Frame_6.BackgroundColor3 = Color3.fromRGB(213, 213, 213)
+Frame_6.BorderSizePixel = 0
+Frame_6.Position = UDim2.new(0.0110207731, 0, 0.164108589, 0)
+Frame_6.Size = UDim2.new(0, 728, 0, 29)
+
+UIStroke_4.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+UIStroke_4.Color = Color3.fromRGB(198, 198, 198)
+UIStroke_4.LineJoinMode = Enum.LineJoinMode.Miter
+UIStroke_4.Thickness = 2.000
+UIStroke_4.Parent = Frame_6
+
+Frame_7.Parent = Frame_2
+Frame_7.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame_7.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame_7.BorderSizePixel = 0
+Frame_7.Position = UDim2.new(0.00968029071, 0, 0.226608604, 0)
+Frame_7.Size = UDim2.new(0, 730, 0, 335)
+
+UIStroke_5.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+UIStroke_5.LineJoinMode = Enum.LineJoinMode.Miter
+UIStroke_5.Parent = Frame_7
+
+UIStroke_6.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+UIStroke_6.Color = Color3.fromRGB(189, 189, 189)
+UIStroke_6.LineJoinMode = Enum.LineJoinMode.Miter
+UIStroke_6.Thickness = 2.000
+UIStroke_6.Parent = Frame_2
