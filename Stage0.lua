8c2c40d7cd5321b9648c1873e944d64d966ce5a8
DevsHubCreator <95543850+DevsHubCreator@users.noreply.github.com>
2022-12-23 20:48:35 +0300

    Update Stage0.lua

--- Stage0.lua
+++ Stage0.lua
@@ -27,6 +27,7 @@ local UIStroke_4 = Instance.new("UIStroke")
 local Frame_7 = Instance.new("Frame")
 local UIStroke_5 = Instance.new("UIStroke")
 local UIStroke_6 = Instance.new("UIStroke")
+local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
 
 --Properties:
 
@@ -37,39 +38,41 @@ Frame.Parent = Windows
 Frame.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
 Frame.BorderSizePixel = 0
 Frame.Position = UDim2.new(2.27573285e-08, 0, -2.30495303e-08, 0)
-Frame.Size = UDim2.new(0, 1341, 0, 662)
+Frame.Size = UDim2.new(1, 0, 1, 0)
 
 ImageLabel.Parent = Frame
 ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 ImageLabel.BackgroundTransparency = 1.000
 ImageLabel.BorderSizePixel = 0
 ImageLabel.Position = UDim2.new(0, 0, -0.0966767371, 0)
-ImageLabel.Size = UDim2.new(0, 1341, 0, 726)
+ImageLabel.Size = UDim2.new(1, 0, 1.09667671, 0)
 ImageLabel.Image = "http://www.roblox.com/asset/?id=6122179491"
 
 Frame_2.Parent = ImageLabel
 Frame_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Frame_2.BorderSizePixel = 0
 Frame_2.Position = UDim2.new(0.221476525, 0, 0.235537186, 0)
-Frame_2.Size = UDim2.new(0, 746, 0, 448)
+Frame_2.Size = UDim2.new(0.556301296, 0, 0.617079914, 0)
 
 Frame_3.Parent = Frame_2
 Frame_3.BackgroundColor3 = Color3.fromRGB(32, 40, 139)
 Frame_3.BorderSizePixel = 0
 Frame_3.Position = UDim2.new(0.0110207731, 0, 0.0167871881, 0)
-Frame_3.Size = UDim2.new(0, 728, 0, 22)
+Frame_3.Size = UDim2.new(0.975871205, 0, 0.0491071381, 0)
 
 TextLabel.Parent = Frame_3
 TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.BackgroundTransparency = 1.000
 TextLabel.BorderSizePixel = 0
 TextLabel.Position = UDim2.new(0.0314207636, 0, 0.18181853, 0)
-TextLabel.Size = UDim2.new(0, 691, 0, 14)
+TextLabel.Size = UDim2.new(0.949175835, 0, 0.636363626, 0)
 TextLabel.ZIndex = 2
 TextLabel.Font = Enum.Font.Code
 TextLabel.Text = "YouTube - Broadcast Yourself. - Microsoft Internet Explorer"
 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
 TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
 TextLabel.TextXAlignment = Enum.TextXAlignment.Left
 
 ImageLabel_2.Parent = Frame_3
@@ -77,7 +80,7 @@ ImageLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 ImageLabel_2.BackgroundTransparency = 1.000
 ImageLabel_2.BorderSizePixel = 0
 ImageLabel_2.Position = UDim2.new(0.00273224711, 0, 0.136363626, 0)
-ImageLabel_2.Size = UDim2.new(0, 15, 0, 15)
+ImageLabel_2.Size = UDim2.new(0.0206043962, 0, 0.681818187, 0)
 ImageLabel_2.Image = "rbxassetid://11899683579"
 ImageLabel_2.SliceScale = 2.000
 
@@ -91,7 +94,7 @@ Frame_4.Parent = Frame_2
 Frame_4.BackgroundColor3 = Color3.fromRGB(213, 213, 213)
 Frame_4.BorderSizePixel = 0
 Frame_4.Position = UDim2.new(0.0110207796, 0, 0.0658942983, 0)
-Frame_4.Size = UDim2.new(0, 728, 0, 22)
+Frame_4.Size = UDim2.new(0.975871205, 0, 0.0491071381, 0)
 
 UIStroke_2.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
 UIStroke_2.Color = Color3.fromRGB(198, 198, 198)
@@ -104,12 +107,14 @@ TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_2.BackgroundTransparency = 1.000
 TextLabel_2.BorderSizePixel = 0
 TextLabel_2.Position = UDim2.new(0.00944275875, 0, 0.136364326, 0)
-TextLabel_2.Size = UDim2.new(0, 75, 0, 14)
+TextLabel_2.Size = UDim2.new(0.103021979, 0, 0.636363626, 0)
 TextLabel_2.ZIndex = 2
 TextLabel_2.Font = Enum.Font.Code
 TextLabel_2.Text = "大声笑(F)"
 TextLabel_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.TextScaled = true
 TextLabel_2.TextSize = 17.000
+TextLabel_2.TextWrapped = true
 TextLabel_2.TextXAlignment = Enum.TextXAlignment.Left
 
 TextLabel_3.Parent = Frame_4
@@ -117,12 +122,14 @@ TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_3.BackgroundTransparency = 1.000
 TextLabel_3.BorderSizePixel = 0
 TextLabel_3.Position = UDim2.new(0.112464741, 0, 0.136364326, 0)
-TextLabel_3.Size = UDim2.new(0, 75, 0, 14)
+TextLabel_3.Size = UDim2.new(0.103021979, 0, 0.636363626, 0)
 TextLabel_3.ZIndex = 2
 TextLabel_3.Font = Enum.Font.Code
 TextLabel_3.Text = "哇哦(E)"
 TextLabel_3.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_3.TextScaled = true
 TextLabel_3.TextSize = 17.000
+TextLabel_3.TextWrapped = true
 TextLabel_3.TextXAlignment = Enum.TextXAlignment.Left
 
 TextLabel_4.Parent = Frame_4
@@ -130,12 +137,14 @@ TextLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_4.BackgroundTransparency = 1.000
 TextLabel_4.BorderSizePixel = 0
 TextLabel_4.Position = UDim2.new(0.203124076, 0, 0.136364326, 0)
-TextLabel_4.Size = UDim2.new(0, 75, 0, 14)
+TextLabel_4.Size = UDim2.new(0.103021979, 0, 0.636363626, 0)
 TextLabel_4.ZIndex = 2
 TextLabel_4.Font = Enum.Font.Code
 TextLabel_4.Text = "非也(V)"
 TextLabel_4.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_4.TextScaled = true
 TextLabel_4.TextSize = 17.000
+TextLabel_4.TextWrapped = true
 TextLabel_4.TextXAlignment = Enum.TextXAlignment.Left
 
 TextLabel_5.Parent = Frame_4
@@ -143,12 +152,14 @@ TextLabel_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_5.BackgroundTransparency = 1.000
 TextLabel_5.BorderSizePixel = 0
 TextLabel_5.Position = UDim2.new(0.296530664, 0, 0.136364326, 0)
-TextLabel_5.Size = UDim2.new(0, 100, 0, 14)
+TextLabel_5.Size = UDim2.new(0.137362644, 0, 0.636363626, 0)
 TextLabel_5.ZIndex = 2
 TextLabel_5.Font = Enum.Font.Code
 TextLabel_5.Text = "男人野兔停止(A)"
 TextLabel_5.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_5.TextScaled = true
 TextLabel_5.TextSize = 17.000
+TextLabel_5.TextWrapped = true
 TextLabel_5.TextXAlignment = Enum.TextXAlignment.Left
 
 TextLabel_6.Parent = Frame_4
@@ -156,12 +167,14 @@ TextLabel_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_6.BackgroundTransparency = 1.000
 TextLabel_6.BorderSizePixel = 0
 TextLabel_6.Position = UDim2.new(0.451750457, 0, 0.136364326, 0)
-TextLabel_6.Size = UDim2.new(0, 68, 0, 14)
+TextLabel_6.Size = UDim2.new(0.0934065953, 0, 0.636363626, 0)
 TextLabel_6.ZIndex = 2
 TextLabel_6.Font = Enum.Font.Code
 TextLabel_6.Text = "ビデオツ(T)"
 TextLabel_6.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_6.TextScaled = true
 TextLabel_6.TextSize = 17.000
+TextLabel_6.TextWrapped = true
 TextLabel_6.TextXAlignment = Enum.TextXAlignment.Left
 
 TextLabel_7.Parent = Frame_4
@@ -169,12 +182,14 @@ TextLabel_7.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_7.BackgroundTransparency = 1.000
 TextLabel_7.BorderSizePixel = 0
 TextLabel_7.Position = UDim2.new(0.57400322, 0, 0.136364326, 0)
-TextLabel_7.Size = UDim2.new(0, 68, 0, 14)
+TextLabel_7.Size = UDim2.new(0.0934065953, 0, 0.636363626, 0)
 TextLabel_7.ZIndex = 2
 TextLabel_7.Font = Enum.Font.Code
 TextLabel_7.Text = "人間だ(H)"
 TextLabel_7.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_7.TextScaled = true
 TextLabel_7.TextSize = 17.000
+TextLabel_7.TextWrapped = true
 TextLabel_7.TextXAlignment = Enum.TextXAlignment.Left
 
 TextLabel_8.Parent = Frame_4
@@ -182,19 +197,21 @@ TextLabel_8.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_8.BackgroundTransparency = 1.000
 TextLabel_8.BorderSizePixel = 0
 TextLabel_8.Position = UDim2.new(0.00944275875, 0, 1.13636434, 0)
-TextLabel_8.Size = UDim2.new(0, 66, 0, 14)
+TextLabel_8.Size = UDim2.new(0.0906593427, 0, 0.636363626, 0)
 TextLabel_8.ZIndex = 2
 TextLabel_8.Font = Enum.Font.Code
 TextLabel_8.Text = "愚か者(D)"
 TextLabel_8.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_8.TextScaled = true
 TextLabel_8.TextSize = 17.000
+TextLabel_8.TextWrapped = true
 TextLabel_8.TextXAlignment = Enum.TextXAlignment.Left
 
 Frame_5.Parent = Frame_2
 Frame_5.BackgroundColor3 = Color3.fromRGB(213, 213, 213)
 Frame_5.BorderSizePixel = 0
 Frame_5.Position = UDim2.new(0.0110207796, 0, 0.11500144, 0)
-Frame_5.Size = UDim2.new(0, 728, 0, 22)
+Frame_5.Size = UDim2.new(0.975871205, 0, 0.0491071381, 0)
 
 UIStroke_3.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
 UIStroke_3.Color = Color3.fromRGB(198, 198, 198)
@@ -206,7 +223,7 @@ Frame_6.Parent = Frame_2
 Frame_6.BackgroundColor3 = Color3.fromRGB(213, 213, 213)
 Frame_6.BorderSizePixel = 0
 Frame_6.Position = UDim2.new(0.0110207731, 0, 0.164108589, 0)
-Frame_6.Size = UDim2.new(0, 728, 0, 29)
+Frame_6.Size = UDim2.new(0.975871205, 0, 0.0647321418, 0)
 
 UIStroke_4.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
 UIStroke_4.Color = Color3.fromRGB(198, 198, 198)
@@ -219,7 +236,7 @@ Frame_7.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Frame_7.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Frame_7.BorderSizePixel = 0
 Frame_7.Position = UDim2.new(0.00968029071, 0, 0.226608604, 0)
-Frame_7.Size = UDim2.new(0, 730, 0, 335)
+Frame_7.Size = UDim2.new(0.978552222, 0, 0.747767806, 0)
 
 UIStroke_5.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
 UIStroke_5.LineJoinMode = Enum.LineJoinMode.Miter
@@ -230,3 +247,6 @@ UIStroke_6.Color = Color3.fromRGB(189, 189, 189)
 UIStroke_6.LineJoinMode = Enum.LineJoinMode.Miter
 UIStroke_6.Thickness = 2.000
 UIStroke_6.Parent = Frame_2
+
+UIAspectRatioConstraint.Parent = Frame_2
+UIAspectRatioConstraint.AspectRatio = 1.665
